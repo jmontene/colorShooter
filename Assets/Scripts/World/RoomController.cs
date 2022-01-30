beecb9fba24184fcf67128ce697f2a4e7d8fc9af
Gamar <gamarazuaje@gmail.com>
2022-01-31 01:41:06 +0900

    Added functional doors to move between rooms

---
+++ Assets/Scripts/World/RoomController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+
+    Room[,] rooms;
+    public int posX, posY;
+
+    public Camera roomCamera; 
+
+    public List<Room> testRooms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rooms = new Room[3,4];
+
+        // Add rooms to array
+        foreach (Room room in testRooms)
+        {
+            var x = room.posX;
+            var y = room.posY;
+            rooms[x,y] = room;
+        }
+
+    }
+
+    public void MoveUp(){
+        posY++;
+        var nextRoom = rooms[posX, posY];
+        Camera.main.transform.position = nextRoom.transform.position;
+        //MovePlayer
+        
+        var player = GameObject.Find("Player").transform.position = new Vector3(nextRoom.transform.position.x, nextRoom.transform.position.y, 1);
+    }
+
+    public void MoveDown(){
+        posY--;
+        Debug.Log("pos:"+posX+"-"+posY);
+        var nextRoom = rooms[posX, posY];
+        Camera.main.transform.position = nextRoom.transform.position;
+
+        Debug.Log("moving down");
+
+        var player = GameObject.Find("Player").transform.position = new Vector3(nextRoom.transform.position.x, nextRoom.transform.position.y, 1);
+    }
+
+    public void MoveLeft(){
+        posX--;
+        var nextRoom = rooms[posX, posY];
+        Camera.main.transform.position = nextRoom.transform.position;
+
+        var player = GameObject.Find("Player").transform.position = new Vector3(nextRoom.transform.position.x, nextRoom.transform.position.y, 1);
+    }
+
+    public void MoveRight(){
+        posX++;
+        var nextRoom = rooms[posX, posY];
+        Camera.main.transform.position = nextRoom.transform.position;
+
+        var player = GameObject.Find("Player").transform.position = new Vector3(nextRoom.transform.position.x, nextRoom.transform.position.y, 1);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
